7141249359527865c3c26e740d5455ac96ff48d9
TomJDz <dzierzakt@gmail.com>
2018-11-18 14:57:40 -0500

    header2

--- header.php
+++ header.php
@@ -1,7 +1,5 @@
-<!-- Header -->
-<header id="header" class="alt">
+
   <a href="index.html" class="logo"><strong>Thomas Dzierzak</strong> <span>Student, Engineer, Innovator</span></a>
   <nav>
     <a href="#menu">Menu</a>
   </nav>
-</header>

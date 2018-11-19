320d8e05c08c5f787514e66b2abfaae8ac052c39
TomJDz <dzierzakt@gmail.com>
2018-11-19 14:58:21 -0500

    better menus

--- nav.php
+++
@@ -1,6 +0,0 @@
-<li><a href="index.html">Home</a></li>
-<li><a href="about.html">About me</a></li>
-<li><a href="schoolwork.html">Schoolwork</a></li>
-<li><a href="hobbies.html">Hobbies</a></li>
-<li><a href="extracurriculars.html">Extracurriculars</a></li>
-<li><a href="work.html">Work</a></li>
